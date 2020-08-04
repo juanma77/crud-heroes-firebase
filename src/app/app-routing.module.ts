c119e1892f2d171b5754fd8a24e23bcd161bed25
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-08-04 17:13:44 -0500

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
 
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+
+
+];
+
+// En los exports van los modulos que vamos a utilizar fuera de este modulo 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
