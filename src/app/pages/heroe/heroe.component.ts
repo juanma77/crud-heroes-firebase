39270c95265466925109f91603e48a1c69363d7c
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-08-05 11:51:29 -0500

    showing message using sweet alert in case of success when updating a heroe

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -3,6 +3,9 @@ import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from 'src/app/services/heroes.service';
 
+import Swal from 'sweetalert2'; 
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -26,27 +29,56 @@ export class HeroeComponent implements OnInit {
 
     }
 
+    Swal.fire({
+
+      title: 'Espere',
+      text: 'Guardando información',
+      allowOutsideClick: false,
+      icon: 'info'
+
+    });
+    Swal.showLoading();
+
     // console.log( form );
     // console.log( this.heroe ); 
+
+    let request : Observable<any>;
 
     // Si ya existe el heroe entonces solo actualizamos su informacion; si no existe el heroe entonces creamos uno nuevo
     if( this.heroe.id ) {
 
-      this.heroesService.updateHeroe( this.heroe ).subscribe( resp =>{
+      request = this.heroesService.updateHeroe( this.heroe ); 
+
+      /*this.heroesService.updateHeroe( this.heroe ).subscribe( resp =>{
 
         console.log( resp );
   
-      });
+      });*/
 
     } else {
 
-      this.heroesService.createNewHeroe( this.heroe ).subscribe( resp =>{
+      request = this.heroesService.createNewHeroe( this.heroe );
+
+      /*this.heroesService.createNewHeroe( this.heroe ).subscribe( resp =>{
 
         console.log( resp );
   
-      });
+      });*/
 
     }
+
+    request.subscribe( resp =>{
+
+      Swal.fire({
+
+        title: this.heroe.name,
+        text: 'Registro actualizado con éxito',
+        allowOutsideClick: false,
+        icon: 'success'
+
+      });
+
+    });
 
   }
 
